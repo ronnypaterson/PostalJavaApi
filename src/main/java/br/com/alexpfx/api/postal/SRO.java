bb1029342a24624eec3c93fbfca449b8da79396f
alexpfx <alexpfba@gmail.com>
2015-02-28 20:09:20 -0300

    consertando problema git.

--- src/main/java/br/com/alexpfx/api/postal/SRO.java
+++ src/main/java/br/com/alexpfx/api/postal/SRO.java
@@ -14,10 +14,10 @@ public class Sro {
     Sro(TipoSro codigoServico, Integer numero, Integer digitoVerificador, String paisOrigem) {
         this.codigoServico = codigoServico;
         this.numero = numero;
-        this.paisOrigem = paisOrigem;
+        this.paisOrigem = paisOrigem.toUpperCase();
         this.digitoVerificador = digitoVerificador;
     }
-
+//teste
     public boolean isValid() throws SroInvalidoException {
         return digitoVerificador.equals(calcularDv());
     }
@@ -65,6 +65,32 @@ public class Sro {
 
     public String getPaisOrigem() {
         return paisOrigem;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Sro sro = (Sro) o;
+
+        if (codigoServico != sro.codigoServico) return false;
+        if (digitoVerificador != null ? !digitoVerificador.equals(sro.digitoVerificador) : sro.digitoVerificador != null)
+            return false;
+        if (numero != null ? !numero.equals(sro.numero) : sro.numero != null) return false;
+        if (paisOrigem != null ? !paisOrigem.equals(sro.paisOrigem) : sro.paisOrigem != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = codigoServico != null ? codigoServico.hashCode() : 0;
+        result = 31 * result + (numero != null ? numero.hashCode() : 0);
+        result = 31 * result + (digitoVerificador != null ? digitoVerificador.hashCode() : 0);
+        result = 31 * result + (paisOrigem != null ? paisOrigem.hashCode() : 0);
+        return result;
     }
 
     @Override
